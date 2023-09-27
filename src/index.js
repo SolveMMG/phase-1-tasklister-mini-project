2f49258f505d92e79d5d80c6450c11a0abf0d335
Your Name <mikachupichu@gmail.com>
2023-09-27 23:29:53 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+// Getting references to html objects.
+const form = document.getElementById("Build");
+const input = document.getElementById("task");
+const taskList = document.getElementById("tasks");
+
+//Deletion function.
+function Delete(event) {
+  const listItem = event.target.parentElement; // Get the parent element
+  taskList.removeChild(listItem); // Remove the parent element
+}
+
+
+// Add an event listener to the form for form submission
+form.addEventListener("submit", function (event) {
+  // Prevent the default form behaviour.
+  event.preventDefault();
+  // Get value of input by user.
+  const taskDescription = input.value ;
+  // Create a new list item .
+  const list = document.createElement("li");
+  // Set the text content of the list item to the task description
+  list.textContent = taskDescription;
+  // Create a delete button
+  const deleteButton = document.createElement("button");
+  deleteButton.textContent = " x ";
+  // Add a click event listener.
+  deleteButton.addEventListener("click", Delete);
+  // Append the delete button.
+  list.appendChild(deleteButton);
+   // Append the new list.
+  taskList.appendChild(list);
+  // Clear the input area.
+  input.value = "";
 });
